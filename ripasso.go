efe25a8a799f9b309b13e6240fce64f42ab721c9
Leonardo Tedone <leonardo.tedone@icloud.com>
2021-02-12 17:22:51 +0100

    init

--- ripasso.go
+++ ripasso.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Scan(&n, &k)
 	fmt.Println(Potenza(n, k))
 	fmt.Println(Max(g))
+	Prova2()
 }
 
 func Potenza(n, k int) int {
@@ -34,4 +35,28 @@ func Max(g []int) int {
 	}
 	fmt.Println(minimo)
 	return massimo
+}
+
+/*func Prova() {
+	var n int = 4
+	var a [n]int
+
+	for i := 0; i < n; i++ {
+		a[i] = i
+	}
+	fmt.Println(a)
+}*/
+
+func Prova2() {
+
+	var a [6]int
+
+	for i, v := range a {
+		v = i
+	}
+
+	for _, v := range a {
+		v *= 2
+	}
+	fmt.Println(a)
 }
